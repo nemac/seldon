0759f1742839ab492ded7227936ad64329e6a88c
Derek Morgan <geoderek@git>
2013-04-10 15:53:08 -0400

    FCAV2 initial load too slow in IE8 & Closing the Map Layers accordion  issue #61 & issue #45 respectively

--- fcav.js
+++ fcav.js
@@ -148,12 +148,15 @@
         };
 
         this.setTheme = function(theme, options) {
-            var app = this,
+            //console.log("setTheme1: "+Date());
+			var app = this,
                 accordionGroup;
 
             if ($("#layerPickerAccordion").data('listAccordion')) {
                 $('#layerPickerAccordion').listAccordion('clearSections');
             }
+			
+			//console.log("setTheme2: "+Date());
 /*
             $('#layerPickerAccordion').empty();
             if ($("#layerPickerAccordion").data('listAccordion')) {
@@ -173,8 +176,12 @@
             if (options === undefined) {
                 options = {};
             }
-
-            $.each(theme.accordionGroups, function (i, accGp) {
+			//console.log("setTheme3: "+Date());
+			
+			//jdm: re-wrote loop using traditional for loops (more vintage-IE friendly)
+			//vintage-IE does work with jquery each loops, but seems to be slower
+			for (var a = 0, b = theme.accordionGroups.length; a < b; a++) {
+				var accGp = theme.accordionGroups[a];
                 // Decide whether to open this accordion group.  If we received an
                 // `accordionGroup` setting in the options are, activate this accordion
                 // group only if it equals that setting.  If we did not receive an
@@ -186,15 +193,16 @@
                     accordionGroup = accGp;
                 }
                 var g = $('#layerPickerAccordion').listAccordion('addSection', '<a>'+accGp.label+'</a>');
-                $.each(accGp.sublists, function (j, sublist) {
-                    var s = $('#layerPickerAccordion').listAccordion('addSublist', g, sublist.label);
-                    $.each(sublist.layers, function (k, layer) {
+				for (var i = 0, j = accGp.sublists.length; i < j; i++) {
+					var sublist = accGp.sublists[i];
+					var s = $('#layerPickerAccordion').listAccordion('addSublist', g, sublist.label);
+					for (var k = 0, l = sublist.layers.length; k < l; k++) {
+						var layer = sublist.layers[k];
                         // remove any previously defined listeners for this layer, in case this isn't the first
                         // time we've been here
                         layer.removeAllListeners("activate");
                         layer.removeAllListeners("deactivate");
                         layer.removeAllListeners("transparency");
-
                         // listen for changes to this layer, and update share url accordingly
                         layer.addListener("activate", function () {
                             app.updateShareMapUrl();
@@ -205,7 +213,6 @@
                         layer.addListener("transparency", function () {
                             app.updateShareMapUrl();
                         });
-
                         // add the layer to the accordion group
                         $('#layerPickerAccordion').listAccordion('addSublistItem', s,
                                                                  [createLayerToggleCheckbox(layer),
@@ -219,10 +226,64 @@
                             ||
                             ((options.layers === undefined) && layer.selectedInConfig)) {
                             layer.activate();
-                        }
-                    });
-                });
-            });
+                        }						
+					}
+				}
+            }
+		  
+		  //jdm: orginal jquery loop	
+          // $.each(theme.accordionGroups, function (i, accGp) {
+                // // Decide whether to open this accordion group.  If we received an
+                // // `accordionGroup` setting in the options are, activate this accordion
+                // // group only if it equals that setting.  If we did not receive an
+                // // `accordionGroup` setting in the options are, activate this accordion
+                // // group if its "selected" attribute was true in the config file.
+                // if ((options.accordionGroup && (accGp === options.accordionGroup))
+                    // ||
+                    // (!options.accordionGroup && accGp.selectedInConfig)) {
+                    // accordionGroup = accGp;
+                // }
+                // var g = $('#layerPickerAccordion').listAccordion('addSection', '<a>'+accGp.label+'</a>');
+                // $.each(accGp.sublists, function (j, sublist) {
+                    // var s = $('#layerPickerAccordion').listAccordion('addSublist', g, sublist.label);
+                    // $.each(sublist.layers, function (k, layer) {
+                        // // remove any previously defined listeners for this layer, in case this isn't the first
+                        // // time we've been here
+                        // layer.removeAllListeners("activate");
+                        // layer.removeAllListeners("deactivate");
+                        // layer.removeAllListeners("transparency");
+
+                        // // listen for changes to this layer, and update share url accordingly
+                        // layer.addListener("activate", function () {
+                            // app.updateShareMapUrl();
+                        // });
+                        // layer.addListener("deactivate", function () {
+                            // app.updateShareMapUrl();
+                        // });
+                        // layer.addListener("transparency", function () {
+                            // app.updateShareMapUrl();
+                        // });
+
+                        // // add the layer to the accordion group
+                        // $('#layerPickerAccordion').listAccordion('addSublistItem', s,
+                                                                 // [createLayerToggleCheckbox(layer),
+                                                                  // $('<label for="chk'+layer.lid+'">'+layer.name+'</label>'),
+                                                                  // createLayerPropertiesIcon(layer)]);
+                        // // Decide whether to activate the layer.  If we received a layer list in the
+                        // // options arg, active the layer only if it appears in that list.  If we
+                        // // received no layer list in the options arg, activate the layer if the layer's
+                        // // "selected" attribute was true in the config file.
+                        // if (((options.layers !== undefined) && (arrayContainsElement(options.layers, layer)))
+                            // ||
+                            // ((options.layers === undefined) && layer.selectedInConfig)) {
+                            // layer.activate();
+                        // }
+                    // });
+                // });
+            // });			
+			
+			
+			//console.log("setTheme4: "+Date());
             if (!accordionGroup) {
                 // if we get to this point and don't have an accordion group to open,
                 // default to the first one
@@ -233,6 +294,7 @@
             $('#layerPickerDialog').scrollTop(0);
             $('#mapToolsDialog').scrollTop(0);
             app.emit("themechange");
+			//console.log("setTheme5: "+Date());
         };
 
         this.shareUrl = function() {
@@ -343,7 +405,7 @@
             $("#layerPickerDialog").dialog({ zIndex:10050, 
                                              position:"left",
                                              autoOpen: true,
-                                             hide:"explode"
+                                             hide:"fade"
                                            });
             app.addListener("accordiongroupchange", function() {
                 if (app.currentTheme) {
@@ -396,7 +458,7 @@
             $("#mapToolsDialog").dialog({ zIndex:10050, 
                                           position:"right",
                                           autoOpen: true,
-                                          hide:"explode"
+                                          hide:"fade"
                                         });
             app.addListener("themechange", function () {
                 app.updateShareMapUrl();
@@ -717,7 +779,8 @@
         };
 
         this.parseConfig = function(configXML, shareUrlInfo) {
-            var app = this;
+            //console.log("start parseConfig: "+Date());
+			var app = this;
             var $configXML = $(configXML),
                 initialBaseLayer,
                 initialTheme,
@@ -864,12 +927,14 @@
 			//Now assuming street maps is always init base layer
 			//comes from: initialBaseLayer.url + '?f=json&pretty=true'
 			var baseLayerInfo = new Object({"currentVersion":10.01,"serviceDescription":"This worldwide street map presents highway-level data for the world. Street-level data includes the United States; much of Canada; Japan; most countries in Europe; Australia and New Zealand; India; parts of South America including Argentina, Brazil, Chile, Colombia, and Venezuela; and parts of southern Africa including Botswana, Lesotho, Namibia, South Africa, and Swaziland.\nThis comprehensive street map includes highways, major roads, minor roads, one-way arrow indicators, railways, water features, administrative boundaries, cities, parks, and landmarks, overlaid on shaded relief imagery for added context. The map also includes building footprints for selected areas in the United States and Europe. Coverage is provided down to ~1:4k with ~1:1k and ~1:2k data available in select urban areas.\nThe street map was developed by Esri using Esri basemap data, DeLorme basemap layers, U.S. Geological Survey (USGS) elevation data, Intact Forest Landscape (IFL) data for the world; NAVTEQ data for Europe, Australia and New Zealand, India, North America, South America (Argentina, Brazil, Chile, Colombia, and Venezuela), and parts of southern Africa (Botswana, Lesotho, Namibia, South Africa, and Swaziland).\n\nFor more information on this map, including our terms of use, visit us \u003ca href=\"http://goto.arcgisonline.com/maps/World_Street_Map \" target=\"_new\"\u003eonline\u003c/a\u003e.","mapName":"Layers","description":"This worldwide street map presents highway-level data for the world. Street-level data includes the United States; much of Canada; Japan; most countries in Europe; Australia and New Zealand; India; parts of South America including Argentina, Brazil, Chile, Colombia, and Venezuela; and parts of southern Africa including Botswana, Lesotho, Namibia, South Africa, and Swaziland.\nThis comprehensive street map includes highways, major roads, minor roads, one-way arrow indicators, railways, water features, administrative boundaries, cities, parks, and landmarks, overlaid on shaded relief imagery for added context. The map also includes building footprints for selected areas in the United States and Europe. Coverage is provided down to ~1:4k with ~1:1k and ~1:2k data available in select urban areas.\nThe street map was developed by Esri using Esri basemap data, DeLorme basemap layers, U.S. Geological Survey (USGS) elevation data, Intact Forest Landscape (IFL) data for the world; NAVTEQ data for Europe, Australia and New Zealand, India, North America, South America (Argentina, Brazil, Chile, Colombia, and Venezuela), and parts of southern Africa (Botswana, Lesotho, Namibia, South Africa, and Swaziland).\n\nFor more information on this map, including the terms of use, visit us online at http://goto.arcgisonline.com/maps/World_Street_Map","copyrightText":"Sources: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012","layers":[{"id":0,"name":"World Street Map","parentLayerId":-1,"defaultVisibility":true,"subLayerIds":null,"minScale":0,"maxScale":0}],"tables":[],"spatialReference":{"wkid":102100},"singleFusedMapCache":true,"tileInfo":{"rows":256,"cols":256,"dpi":96,"format":"JPEG","compressionQuality":90,"origin":{"x":-20037508.342787,"y":20037508.342787},"spatialReference":{"wkid":102100},"lods":[{"level":0,"resolution":156543.033928,"scale":591657527.591555},{"level":1,"resolution":78271.5169639999,"scale":295828763.795777},{"level":2,"resolution":39135.7584820001,"scale":147914381.897889},{"level":3,"resolution":19567.8792409999,"scale":73957190.948944},{"level":4,"resolution":9783.93962049996,"scale":36978595.474472},{"level":5,"resolution":4891.96981024998,"scale":18489297.737236},{"level":6,"resolution":2445.98490512499,"scale":9244648.868618},{"level":7,"resolution":1222.99245256249,"scale":4622324.434309},{"level":8,"resolution":611.49622628138,"scale":2311162.217155},{"level":9,"resolution":305.748113140558,"scale":1155581.108577},{"level":10,"resolution":152.874056570411,"scale":577790.554289},{"level":11,"resolution":76.4370282850732,"scale":288895.277144},{"level":12,"resolution":38.2185141425366,"scale":144447.638572},{"level":13,"resolution":19.1092570712683,"scale":72223.819286},{"level":14,"resolution":9.55462853563415,"scale":36111.909643},{"level":15,"resolution":4.77731426794937,"scale":18055.954822},{"level":16,"resolution":2.38865713397468,"scale":9027.977411},{"level":17,"resolution":1.19432856685505,"scale":4513.988705},{"level":18,"resolution":0.597164283559817,"scale":2256.994353},{"level":19,"resolution":0.298582141647617,"scale":1128.497176}]},"initialExtent":{"xmin":-28872328.0888923,"ymin":-11237732.4896886,"xmax":28872328.0888923,"ymax":11237732.4896886,"spatialReference":{"wkid":102100}},"fullExtent":{"xmin":-20037507.0671618,"ymin":-19971868.8804086,"xmax":20037507.0671618,"ymax":19971868.8804086,"spatialReference":{"wkid":102100}},"units":"esriMeters","supportedImageFormatTypes":"PNG24,PNG,JPG,DIB,TIFF,EMF,PS,PDF,GIF,SVG,SVGZ,AI,BMP","documentInfo":{"Title":"World Street Map","Author":"Esri","Comments":"","Subject":"streets, highways, major roads, railways, water features, administrative boundaries, cities, parks, protected areas, landmarks ","Category":"transportation(Transportation Networks) ","Keywords":"World, Global, Europe, Japan, Hong Kong, North America, United States, Canada, Mexico, Southern Africa, Asia, South America, Australia, New Zealand, India, Argentina, Brazil, Chile, Venezuela, Andorra, Austria, Belgium, Czech Republic, Denmark, France, Germany, Great Britain, Greece, Hungary, Ireland, Italy, Luxembourg, Netherlands, Norway, Poland, Portugal, San Marino, Slovakia, Spain, Sweden, Switzerland, Russia, Thailand, Turkey, 2012","Credits":"Sources: Esri, DeLorme, NAVTEQ, USGS, Intermap, iPC, NRCAN, Esri Japan, METI, Esri China (Hong Kong), Esri (Thailand), TomTom, 2012"},"capabilities":"Map"});
+			//console.log("end parseConfig: "+Date());
 			app.initOpenLayers(baseLayerInfo, initialBaseLayer, initialTheme, themeOptions, initialExtent);
-
+	
         };
 
         this.initOpenLayers = function(baseLayerInfo, baseLayer, theme, themeOptions, initialExtent) {
-            var layer = new OpenLayers.Layer.ArcGISCache("AGSCache", baseLayer.url, {
+            //console.log("start initOpenLayers: "+Date());
+			var layer = new OpenLayers.Layer.ArcGISCache("AGSCache", baseLayer.url, {
                 layerInfo: baseLayerInfo
             });
             
@@ -923,6 +988,7 @@
                 OpenLayers.Util.getElement("latLonTracker").innerHTML = "Lat: " + sprintf("%.5f", lonlat.lat) + " Lon: " + sprintf("%.5f", lonlat.lon) + "";
             });
 			app.map.addControl(new OpenLayers.Control.PanZoomBar());
+			//console.log("end initOpenLayers: "+Date());
         };
 
     };
@@ -1061,7 +1127,7 @@
     }
 
     function displayError(message) {
-        console.log(message);
+        //console.log(message);
     }
 
     fcav.init = function(config,projection,gisServerType) {
@@ -1534,7 +1600,7 @@
                                 });
                             },
                             error: function(jqXHR, textStatus, errorThrown) {
-                                console.log('got error');
+                                //console.log('got error');
                                 alert(textStatus);
                             }
                         });
