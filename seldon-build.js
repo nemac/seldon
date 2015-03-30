e85c96731fcc5b07c23a95bdd34ab0fb139bd5a9
John Frimmel <jrfrimme@unca.edu>
2015-03-30 12:39:24 -0400

    abstracts print code

--- seldon-build.js
+++ seldon-build.js
@@ -306,7 +306,7 @@ module.exports = function ($, app) {
     return createIdentifyTool;
 }
 
-},{"./clicktool.js":1,"./stringContainsChar.js":10}],3:[function(require,module,exports){
+},{"./clicktool.js":1,"./stringContainsChar.js":11}],3:[function(require,module,exports){
 module.exports = function ($, app) {
     function Layer (settings) {
         EventEmitter.call(this);
@@ -729,6 +729,80 @@ module.exports = function ($, app) {
 }
 
 },{"./clicktool.js":1}],8:[function(require,module,exports){
+module.exports = function ($, app) {
+    function printMap () {
+        // go through all layers, and collect a list of objects
+        // each object is a tile's URL and the tile's pixel location relative to the viewport
+        var offsetX = parseInt(app.map.layerContainerDiv.style.left, 10);
+        var offsetY = parseInt(app.map.layerContainerDiv.style.top, 10);
+        var size  = app.map.getSize();
+        var tiles = [];
+        for (var i = 0; i < app.map.layers.length; ++i) {
+            // if the layer isn't visible at this range, or is turned off, skip it
+            try {
+                var layer = app.map.layers[i];
+                if (!layer.getVisibility()) continue;
+                if (!layer.calculateInRange()) continue;
+                // iterate through their grid's tiles, collecting each tile's extent and pixel location at this moment
+                // for (var tilerow in layer.grid) {
+                for (var j = 0; j < layer.grid.length; ++j) {
+                    // for (var tilei in layer.grid[tilerow]) {
+                    for (var k = 0; k < layer.grid[j].length; ++k) {
+                        var tile     = layer.grid[j][k];
+                        var url      = layer.getURL(tile.bounds);
+                        var position = tile.position;
+                        var tilexpos = position.x + offsetX;
+                        var tileypos = position.y + offsetY;
+                        var opacity  = layer.opacity ? parseInt(100*layer.opacity, 10) : 100;
+                        tiles[tiles.length] = {url:url, x:tilexpos, y:tileypos, opacity:opacity};
+                    }
+                }
+            } //end try
+            catch(err) {
+                alert(err.message);
+            }
+        }
+
+        //the legend url's to pass along
+        var layerLegendsURLs   = [];
+        $('#legend').find('div').each(function(){
+            var innerDivId = $(this).attr('id');
+            var innerDivImgSrc = $('#'+innerDivId).children('img').attr('src');
+            layerLegendsURLs[layerLegendsURLs.length]= {url:innerDivImgSrc};
+        });
+
+        // hand off the list to our server-side script, which will do the heavy lifting
+        var tiles_json = JSON.stringify(tiles);
+        var legends_json = JSON.stringify(layerLegendsURLs);
+        // var printparams = 'width='+size.w + '&height='+size.h + '&tiles='+escape(tiles_json) ;
+
+        var printPopup = $(document.createElement('div'));
+        printPopup.id = "#printPopupDiv";
+        printPopup.html('<div id="printMapLoader"><center><img class="ajax-loader-image" src="icons/ajax-loader.gif"/></center></div>');
+        printPopup.dialog({
+            resizable : false,
+            height    : 75,
+            title     : 'Creating Image for Print...',
+            close : function( event, ui ) {
+                $(this).remove();
+            }
+        });
+
+        OpenLayers.Request.POST({
+            url: 'http://'+window.location.hostname+window.location.pathname+'cgi-bin/print.cgi',
+            data: OpenLayers.Util.getParameterString({width:size.w,height:size.h,tiles:tiles_json,legends:legends_json}),
+            headers:{'Content-Type':'application/x-www-form-urlencoded'},
+            callback: function(request) {
+                $("#printMapLoader").html('<center><a href="http://'+window.location.hostname+window.location.pathname+'cgi-bin/printed_map.jpg" style="color:blue" target="_new">print image result</a></center>');
+                printPopup.dialog('option', 'title', 'Print Image Created!');
+            }
+        });
+    }
+
+    return printMap;
+};
+
+},{}],9:[function(require,module,exports){
 function ShareUrlInfo (settings) {
     if (settings === undefined) {
         settings = {};
@@ -838,7 +912,7 @@ ShareUrlInfo.prototype.urlArgs = function () {
 
 module.exports = ShareUrlInfo;
 
-},{}],9:[function(require,module,exports){
+},{}],10:[function(require,module,exports){
 module.exports = function ($) {
     function createSplashScreen () {
         var $splashScreenContainer = $("#splashScreenContainer"),
@@ -859,14 +933,14 @@ module.exports = function ($) {
     return createSplashScreen;
 }
 
-},{}],10:[function(require,module,exports){
+},{}],11:[function(require,module,exports){
 function stringContainsChar (string, c) {
     return (string.indexOf(c) >= 0);
 }
 
 module.exports = stringContainsChar;
 
-},{}],11:[function(require,module,exports){
+},{}],12:[function(require,module,exports){
 function Theme (settings) {
     this.accordionGroups = [];
     if (!settings) { return; }
@@ -893,7 +967,7 @@ function Theme (settings) {
 
 module.exports = Theme;
 
-},{}],12:[function(require,module,exports){
+},{}],13:[function(require,module,exports){
 (function ($) {
     "use strict";
 
@@ -2662,75 +2736,8 @@ module.exports = Theme;
         }
     }));
 
-    function printMap () {
-        // go through all layers, and collect a list of objects
-        // each object is a tile's URL and the tile's pixel location relative to the viewport
-        var offsetX = parseInt(app.map.layerContainerDiv.style.left);
-        var offsetY = parseInt(app.map.layerContainerDiv.style.top);
-        var size  = app.map.getSize();
-        var tiles = [];
-        for (var i = 0; i < app.map.layers.length; ++i) {
-            // if the layer isn't visible at this range, or is turned off, skip it
-            try {
-                var layer = app.map.layers[i];
-                if (!layer.getVisibility()) continue;
-                if (!layer.calculateInRange()) continue;
-                // iterate through their grid's tiles, collecting each tile's extent and pixel location at this moment
-                // for (var tilerow in layer.grid) {
-                for (var j = 0; j < layer.grid.length; ++j) {
-                    // for (var tilei in layer.grid[tilerow]) {
-                    for (var k = 0; k < layer.grid[j].length; ++k) {
-                        var tile     = layer.grid[j][k]
-                            var url      = layer.getURL(tile.bounds);
-                        var position = tile.position;
-                        var tilexpos = position.x + offsetX;
-                        var tileypos = position.y + offsetY;
-                        var opacity  = layer.opacity ? parseInt(100*layer.opacity) : 100;
-                        tiles[tiles.length] = {url:url, x:tilexpos, y:tileypos, opacity:opacity};
-                    }
-                }
-            } //end try
-            catch(err) {
-                alert(err.message);
-            }
-        }
-
-        //the legend url's to pass along
-        var layerLegendsURLs   = [];
-        $('#legend').find('div').each(function(){
-            var innerDivId = $(this).attr('id');
-            var innerDivImgSrc = $('#'+innerDivId).children('img').attr('src');
-            layerLegendsURLs[layerLegendsURLs.length]= {url:innerDivImgSrc};
-        });
-
-        // hand off the list to our server-side script, which will do the heavy lifting
-        var tiles_json = JSON.stringify(tiles);
-        var legends_json = JSON.stringify(layerLegendsURLs);
-        // var printparams = 'width='+size.w + '&height='+size.h + '&tiles='+escape(tiles_json) ;
-
-        var printPopup = $(document.createElement('div'));
-        printPopup.id = "#printPopupDiv";
-        printPopup.html('<div id="printMapLoader"><center><img class="ajax-loader-image" src="icons/ajax-loader.gif"/></center></div>');
-        printPopup.dialog({
-            resizable : false,
-            height    : 75,
-            title     : 'Creating Image for Print...',
-            close : function( event, ui ) {
-                $(this).remove();
-            },
-        });
-
-        OpenLayers.Request.POST({
-            url: 'http://'+window.location.hostname+window.location.pathname+'cgi-bin/print.cgi',
-            data: OpenLayers.Util.getParameterString({width:size.w,height:size.h,tiles:tiles_json,legends:legends_json}),
-            headers:{'Content-Type':'application/x-www-form-urlencoded'},
-            callback: function(request) {
-                $("#printMapLoader").html('<center><a href="http://'+window.location.hostname+window.location.pathname+'cgi-bin/printed_map.jpg" style="color:blue" target="_new">print image result</a></center>');
-                printPopup.dialog('option', 'title', 'Print Image Created!');
-            }
-        });
-    }
+    var printMap = require('./js/print.js')($, app);
 
 }(jQuery));
 
-},{"./js/clicktool.js":1,"./js/identify.js":2,"./js/layer.js":3,"./js/layer_checkbox.js":4,"./js/layer_dialog.js":5,"./js/layer_icon.js":6,"./js/multigraph.js":7,"./js/share.js":8,"./js/splash.js":9,"./js/stringContainsChar.js":10,"./js/theme.js":11}]},{},[12]);
+},{"./js/clicktool.js":1,"./js/identify.js":2,"./js/layer.js":3,"./js/layer_checkbox.js":4,"./js/layer_dialog.js":5,"./js/layer_icon.js":6,"./js/multigraph.js":7,"./js/print.js":8,"./js/share.js":9,"./js/splash.js":10,"./js/stringContainsChar.js":11,"./js/theme.js":12}]},{},[13]);
