08a20adac7f9da84fd77473d73b71549eba2f530
John Frimmel <jrfrimme@unca.edu>
2015-04-06 15:01:27 -0400

    cleans up base layer code

--- seldon.js
+++ seldon.js
@@ -2072,11 +2072,7 @@ module.exports = function ($) {
         var app = this;
         if (baseLayer.name.indexOf("Google") > -1) {
             var layer = new OpenLayers.Layer.Google("Google Streets");
-            app.map.removeLayer(app.map.layers[0]);
-            app.currentBaseLayer = baseLayer;
-            app.map.addLayers([layer]);
-            app.map.setLayerIndex(layer, 0);
-            app.emit("baselayerchange");
+            handleBaseLayer(app, layer, baseLayer);
         } else { //assuming esri base layer at this point
             $.ajax({
                 url: baseLayer.url + '?f=json&pretty=true',
@@ -2085,17 +2081,21 @@ module.exports = function ($) {
                     var layer = new OpenLayers.Layer.ArcGISCache("AGSCache", baseLayer.url, {
                         layerInfo: layerInfo
                     });
-                    app.map.removeLayer(app.map.layers[0]);
-                    app.currentBaseLayer = baseLayer;
-                    app.map.addLayers([layer]);
-                    app.map.setLayerIndex(layer, 0);
-                    app.emit("baselayerchange");
+                    handleBaseLayer(app, layer, baseLayer);
                 },
                 error: function(jqXHR, textStatus, errorThrown) {
                     alert(textStatus);
                 }
             });
         }
+    }
+
+    function handleBaseLayer (app, layer, baseLayer) {
+        app.map.removeLayer(app.map.layers[0]);
+        app.currentBaseLayer = baseLayer;
+        app.map.addLayers([layer]);
+        app.map.setLayerIndex(layer, 0);
+        app.emit("baselayerchange");
     }
 
     return setBaseLayer;
