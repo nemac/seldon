261eb9e75272d43bdd2a8d73ea4a01371c4717d5
John Frimmel <jrfrimme@unca.edu>
2015-04-06 14:54:38 -0400

    cleans up print code

--- js/print.js
+++ js/print.js
@@ -6,24 +6,27 @@ module.exports = function ($, app) {
         var offsetY = parseInt(app.map.layerContainerDiv.style.top, 10);
         var size  = app.map.getSize();
         var tiles = [];
-        for (var i = 0; i < app.map.layers.length; ++i) {
+        var layer, tile, position;
+        var i, j, k;
+
+        for (i = 0; i < app.map.layers.length; ++i) {
             // if the layer isn't visible at this range, or is turned off, skip it
             try {
-                var layer = app.map.layers[i];
+                layer = app.map.layers[i];
                 if (!layer.getVisibility()) continue;
                 if (!layer.calculateInRange()) continue;
                 // iterate through their grid's tiles, collecting each tile's extent and pixel location at this moment
                 // for (var tilerow in layer.grid) {
-                for (var j = 0; j < layer.grid.length; ++j) {
-                    // for (var tilei in layer.grid[tilerow]) {
-                    for (var k = 0; k < layer.grid[j].length; ++k) {
-                        var tile     = layer.grid[j][k];
-                        var url      = layer.getURL(tile.bounds);
-                        var position = tile.position;
-                        var tilexpos = position.x + offsetX;
-                        var tileypos = position.y + offsetY;
-                        var opacity  = layer.opacity ? parseInt(100*layer.opacity, 10) : 100;
-                        tiles[tiles.length] = {url:url, x:tilexpos, y:tileypos, opacity:opacity};
+                for (j = 0; j < layer.grid.length; ++j) {
+                    for (k = 0; k < layer.grid[j].length; ++k) {
+                        tile     = layer.grid[j][k];
+                        position = tile.position;
+                        tiles.push({
+                            url     : layer.getURL(tile.bounds),
+                            x       : position.x + offsetX,
+                            y       : position.y + offsetY,
+                            opacity : layer.opacity ? parseInt(100*layer.opacity, 10) : 100
+                        });
                     }
                 }
             } //end try
@@ -33,26 +36,23 @@ module.exports = function ($, app) {
         }
 
         //the legend url's to pass along
-        var layerLegendsURLs   = [];
-        $('#legend').find('div').each(function(){
-            var innerDivId = $(this).attr('id');
-            var innerDivImgSrc = $('#'+innerDivId).children('img').attr('src');
-            layerLegendsURLs[layerLegendsURLs.length]= {url:innerDivImgSrc};
+        var layerLegendsURLs = [];
+        $('#legend').find('div').each(function () {
+            var innerDivImgSrc = $(this).children('img').attr('src');
+            layerLegendsURLs.push({ url: innerDivImgSrc });
         });
 
         // hand off the list to our server-side script, which will do the heavy lifting
         var tiles_json = JSON.stringify(tiles);
         var legends_json = JSON.stringify(layerLegendsURLs);
-        // var printparams = 'width='+size.w + '&height='+size.h + '&tiles='+escape(tiles_json) ;
-
         var printPopup = $(document.createElement('div'));
-        printPopup.id = "#printPopupDiv";
+        printPopup.id = "printPopupDiv";
         printPopup.html('<div id="printMapLoader"><center><img class="ajax-loader-image" src="icons/ajax-loader.gif"/></center></div>');
         printPopup.dialog({
             resizable : false,
             height    : 75,
             title     : 'Creating Image for Print...',
-            close : function( event, ui ) {
+            close     : function (event, ui) {
                 $(this).remove();
             }
         });
