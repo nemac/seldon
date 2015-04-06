371fa947f3cac581a26ba43a637b784fd1aff29c
John Frimmel <jrfrimme@unca.edu>
2015-04-06 16:11:38 -0400

    cleans up mask code some

--- js/set_mask_by_layer.js
+++ js/set_mask_by_layer.js
@@ -3,47 +3,54 @@ module.exports = function ($) {
         var Layer = require("./layer.js")($, this);
 
         var app = this;
+        var maskLayer, maskName, cleanMaskName;
+        var m, ml, mld;
 
         if (toggle) {
             app.maskParentLayers.push(parentLayer);
-            for (var m = 0; m < app.masks.length; m++) {
-                var maskLayer = new Layer({
-                    lid              : parentLayer.lid+app.masks[m].maskName.replace("/",""),
-                    visible          : 'true',
-                    url              : parentLayer.url,
-                    srs              : parentLayer.srs,
-                    layers           : parentLayer.layers+app.masks[m].maskName.replace("/",""),
-                    identify         : parentLayer.identify,
-                    name             : parentLayer.lid+app.masks[m].maskName.replace("/",""),
-                    mask             : 'false',
-                    legend           : parentLayer.legend,
-                    index            : parentLayer.index
+            for (m = 0; m < app.masks.length; m++) {
+                maskName = app.masks[m].maskName;
+                cleanMaskName = maskName.replace("/","");
+                maskLayer = new Layer({
+                    lid         : parentLayer.lid + cleanMaskName,
+                    visible     : 'true',
+                    url         : parentLayer.url,
+                    srs         : parentLayer.srs,
+                    layers      : parentLayer.layers + cleanMaskName,
+                    identify    : parentLayer.identify,
+                    name        : parentLayer.lid + cleanMaskName,
+                    mask        : 'false',
+                    legend      : parentLayer.legend,
+                    index       : parentLayer.index,
+                    parentLayer : parentLayer
                 });
-                maskLayer.parentLayer = parentLayer;
                 maskLayer.activate();
+
                 app.masks[m].maskLayers.push(maskLayer);
+
                 if (parentLayer.visible === "true") {
                     parentLayer.deactivate();
                     parentLayer.visible = "false";
                 }
-                $("#" + app.masks[m].maskName.replace("MaskFor", "")).get(0).checked = true;
-                $('#mask-status' + parentLayer.lid).text("(m)");
+
+                $("#" + maskName.replace("MaskFor", "")).get(0).checked = true;
+                $("#mask-status" + parentLayer.lid).text("(m)");
                 $("#chk" + parentLayer.lid).prop('checked', true);
             }
         } else {
             //deactivate and remove from mask.maskLayers[]
-            for (var m = 0; m < app.masks.length; m++) {
+            for (m = 0; m < app.masks.length; m++) {
                 var currentMask = app.masks[m];
                 var maskLayersToDelete = [];
-                for (var ml = 0; ml < app.masks[m].maskLayers.length; ml++) {
-                    var currentMaskLayer = app.masks[m].maskLayers[ml];
+                for (ml = 0; ml < currentMask.maskLayers.length; ml++) {
+                    var currentMaskLayer = currentMask.maskLayers[ml];
                     if (currentMaskLayer.parentLayer.lid == parentLayer.lid) {
                         currentMaskLayer.deactivate();
                         $('#mask-status'+ currentMaskLayer.parentLayer.lid).text("")
                         maskLayersToDelete.push(currentMaskLayer);
                     }
                 }
-                for (var mld = 0; mld < maskLayersToDelete.length; mld++) {
+                for (mld = 0; mld < maskLayersToDelete.length; mld++) {
                     currentMask.maskLayers.remove(maskLayersToDelete[mld]);
                 }
             }
@@ -52,7 +59,6 @@ module.exports = function ($) {
             if (parentLayer.visible === "false") {
                 parentLayer.visible = "true";
             } else {
-                parentLayer.visible == "true";
                 parentLayer.deactivate();
             }
             $('#mask-status'+ parentLayer.lid).text("");
