ba42a7bd3a31e4622f1b8ccdc4d480815a825ca7
Matt Geiger <matt@git>
2015-09-17 20:14:01 +0000

    Fix issue with tab-focusing shifting position of overflowing div.

--- js/collapsible_sublist_setup.js
+++ js/collapsible_sublist_setup.js
@@ -3,9 +3,17 @@ module.exports = function ($) {
 
         var heightOfLayerDiv = $('.layer').outerHeight(true);
         var initialLessSublistHeightFactor = 10,
-            heightIncFactor = 30,
+            heightIncFactor = 70,
             heightInc = heightOfLayerDiv * heightIncFactor,
-            initialLessSublistHeight = heightOfLayerDiv * initialLessSublistHeightFactor + 'px';
+            initialLessSublistHeight = heightOfLayerDiv * initialLessSublistHeightFactor + 'px',
+            $collapsingSublists = $('.showLessSublist');
+
+        $collapsingSublists.each(function () {
+            var $layerCheckboxes = $(this).find(':checkbox');
+            $layerCheckboxes.slice(0, initialLessSublistHeightFactor).attr('tabIndex', 0);
+            $layerCheckboxes.slice(initialLessSublistHeightFactor).attr('tabIndex', -1);
+        });
+
         
         $('.showLessSublist').css('height', initialLessSublistHeight);
 
@@ -17,32 +25,42 @@ module.exports = function ($) {
             $this.siblings('button.show-more-layers').removeClass('active').prop('disabled', true);
             $this.siblings('button.show-less-layers').addClass('active').prop('disabled', false);
             $sublist.css('height', sublistScrollHeight);
+            $sublist.find(':checkbox').attr('tabIndex', 0);
         });
 
         $('button.show-more-layers').on('click', function (event) {
             var $this = $(this);
-                $this.blur();
             var $sublist = $this.parent().siblings('.layer-group'),
+                $layerCheckboxes = $sublist.find(':checkbox'),
                 heightInPx = parseInt($sublist.css('height').slice(0,-2)),
-                scrollHeightInPx = $sublist.prop('scrollHeight');
+                sublistScrollHeight = $sublist.prop('scrollHeight');
 
-            if (heightInPx+heightInc > scrollHeightInPx) {
+            if (heightInPx+heightInc > sublistScrollHeight) {
                 $sublist.css('height', sublistScrollHeight);
                 $this.removeClass('active').prop('disabled', true);
                 $this.siblings('button.show-all-layers').removeClass('active').prop('disabled', true);
+                $layerCheckboxes.attr('tabIndex', 0);
             } else {
-                $sublist.css('height', '+='+heightInc);
+                var newHeightInPx = parseInt($sublist.css('height').slice(0, -2)) + heightInc;
+                var numVisibleCheckboxes = newHeightInPx / heightOfLayerDiv;
+                $sublist.css('height', newHeightInPx + 'px');
                 $this.siblings('button.show-less-layers').addClass('active').prop('disabled', false);
+                $layerCheckboxes.slice(0, numVisibleCheckboxes).attr('tabIndex', 0);
+                $layerCheckboxes.slice(numVisibleCheckboxes).attr('tabIndex', -1);
             }
 
         });
 
         $('button.show-less-layers').on('click', function (event) {
-            var $this = $(this);
-            $this.parent().siblings('.layer-group').css('height', initialLessSublistHeight);
+            var $this = $(this),
+                $sublist = $this.parent().siblings('.layer-group'),
+                $layerCheckboxes = $sublist.find(':checkbox');
+            $sublist.css('height', initialLessSublistHeight);
             $this.removeClass('active').prop('disabled', true);
             $this.siblings('button.show-more-layers').addClass('active').prop('disabled', false);
             $this.siblings('button.show-all-layers').addClass('active').prop('disabled', false);
+            $layerCheckboxes.slice(0, initialLessSublistHeightFactor).attr('tabIndex', 0);
+            $layerCheckboxes.slice(initialLessSublistHeightFactor).attr('tabIndex', -1);
         });
     }
 
