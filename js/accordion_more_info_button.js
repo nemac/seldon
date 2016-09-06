ad46065aa512acc0ad92b5a37c76e02c8f59ef14
Matthew Geiger <mgeiger@unca.edu>
2016-09-06 12:47:41 -0400

    add more info button file

---
+++ js/accordion_more_info_button.js
@@ -0,0 +1,15 @@
+module.exports = function ($) {
+  function MoreInfoButton(el) {
+    var dialogDiv = '<div class="more-info-dialog"><p>'+el.description+'</p></div>';
+    this.element = document.createElement('button');
+    this.element.textContent = '?';
+    this.element.className = 'accordion-more-info-button';
+    this.element.onclick = function (event) {
+      $(dialogDiv).dialog({
+          title : el.label + ' Info'
+      })
+    }
+  }
+
+  return MoreInfoButton;
+}
