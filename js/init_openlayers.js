15207391e87581c6c4982d9a8817de5b7488f57a
Matthew Geiger <mgeiger@unca.edu>
2021-07-12 17:13:43 +0000

    Fix shifted basemaps

--- js/init_openlayers.js
+++ js/init_openlayers.js
@@ -1,26 +1,5 @@
 function initOpenLayers (baseLayerInfo, baseLayer, theme, themeOptions, initialExtent) {
     var app = this;
-    var resolutions = [
-        156543.03390625,    // 0
-        78271.516953125,    // 1
-        39135.7584765625,   // 2
-        19567.87923828125,  // 3
-        9783.939619140625,  // 4
-        4891.9698095703125, // 5
-        2445.9849047851562, // 6
-        1222.9924523925781, // 7
-        611.4962261962891,  // 8
-        305.74811309814453, // 9
-        152.87405654907226, // 10
-        76.43702827453613,  // l1
-        38.218514137268066, // 12
-        19.109257068634033, // 13
-        9.554628534317017,  // 14
-        4.777314267158508,  // 15
-        2.388657133579254,  // 16
-        1.194328566789627,  // 17
-        0.5971642833948135  // 18
-    ]
     if (baseLayer.type == 'Google') {
         var layer = new OpenLayers.Layer.Google("Google Streets", {numZoomLevels: 20});
     } else if (baseLayer.type == 'WMTS')  {
@@ -31,8 +10,6 @@ function initOpenLayers (baseLayerInfo, baseLayer, theme, themeOptions, initialE
             url: baseLayer.url,
             layer: baseLayer.name,
             matrixSet: baseLayer.tileMatrixSet,
-            sphericalMercator: true,
-            resolutions: resolutions
         }
         if (baseLayer.numZoomLevels) {
             var serverResolutions = resolutions.slice(0, baseLayer.numZoomLevels)
@@ -75,9 +52,7 @@ function initOpenLayers (baseLayerInfo, baseLayer, theme, themeOptions, initialE
     });
 
     app.map = new OpenLayers.Map('map', {
-        maxExtent:         maxExtentBounds,
         units:             'm',
-        resolutions:       layer.resolutions,
         tileManager:       app.tileManager,
         center: [-10986902.689297,4856468.480035],
         controls: [
@@ -106,7 +81,7 @@ function initOpenLayers (baseLayerInfo, baseLayer, theme, themeOptions, initialE
     app.map.setLayerIndex(layer, 0);
     app.setTheme(theme, themeOptions);
 
-    var defaultZoom = 4
+    var defaultZoom = 5
     app.map.setCenter(app.map.getCenter(), defaultZoom)
 
     app.saveCurrentExtent()
